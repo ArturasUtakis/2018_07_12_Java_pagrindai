da296f3e95fbfb2e0b9cac9ef70b377705960d5c
Arturas Utakis <utakis.arturas@gmail.com>
2018-07-13 20:18:51 +0300

    antra uzduotis

--- src/days/second/secondTask.java
+++ src/days/second/secondTask.java
@@ -1,5 +1,26 @@
 package days.second;
 
 public class secondTask {
+    public static void main(String[] args) {
+        String palindrome = "KOL EINU ŠUNIE LOK".replaceAll(" ", "");//replace all() panaikina simboli
+        if(isPalindrome(palindrome)) {
+            System.out.println("Palindromas");
+        }else{
+            System.out.println("Nepalindromas!!!");
+        }
+    }
 
-}
+        private static boolean isPalindrome (String palindrome){
+            boolean result = true;
+            for (int i = 0; i < palindrome.length(); i++) {
+                char a = palindrome.charAt(i);
+                char b = palindrome.charAt(palindrome.length() - i - 1);
+                if (a != b) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+    }
